dfcf46e2d4bb17ea095c7e7bd54a5c1931187f07
Luiz Lelis <luizhlelis@gmail.com>
2022-01-28 00:10:42 -0300

    chore: first lesson iterations

---
+++ codility/src/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace SourceCode
+{
+    public class BinaryGap
+    {
+        public int Solution(int N)
+        {
+			bool started = false;
+			int length = 0, max = 0;
+			for (uint i = 1; i <= N; i <<= 1)
+			{
+				if ((i & N) != 0)
+				{
+					if (started)
+					{
+						if (length > max)
+							max = length;
+						length = 0;
+					}
+					started = true;
+				}
+				else if (started)
+					++length;
+			}
+			return max;
+		}
+    }
+}
---
+++ codility/test/UnitTests.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using SourceCode;
+
+namespace Tests
+{
+    public class UnitTests
+    {
+        [Theory]
+        [InlineData(1041, 5)]
+        [InlineData(529, 4)]
+        [InlineData(20, 1)]
+        [InlineData(32, 0)]
+        [InlineData(15, 0)]
+        public void ShouldReturnBigestBinaryGap(int number, int expectedResponse)
+        {
+            var response = new BinaryGap().Solution(number);
+            Assert.Equal(expectedResponse, response);
+        }
+    }
+}
