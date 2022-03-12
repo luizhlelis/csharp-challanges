de6453782e28ed57392a53f780c51bcdf6a9408d
Luiz Lelis <luizhlelis@gmail.com>
2022-03-11 22:57:13 -0300

    chore: `century` task

--- codesignal/src/Program.cs
+++ codesignal/src/Program.cs
@@ -2,8 +2,19 @@
 
 namespace SourceCode
 {
-    public static class FirstTest
+    public static class Century
     {
+        int Solution(int year)
+        {
+            int century;
+            if (year % 100 < 0)
+                century = 1;
+            else if (year % 100 == 0)
+                century = year / 100;
+            else
+                century = year / 100 + 1;
 
+            return century;
+        }
     }
 }
