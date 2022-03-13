52453a389a1a596b6248259c969c104160286eea
Luiz Lelis <luizhlelis@gmail.com>
2022-03-13 19:52:22 -0300

    chore: `century` tests

--- codesignal/test/UnitTest.cs
+++ codesignal/test/UnitTest.cs
@@ -7,6 +7,17 @@ namespace test
     public class UnitTest1
     {
         [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2022, 21)]
+        [InlineData(2000, 20)]
+        public void CenturyTest(int year, int expectedResponse)
+        {
+            var response = new Century().Solution(year);
+
+            Assert.Equal(expectedResponse, response);
+        }
+
+        [Theory]
         [InlineData("aabaa", true)]
         [InlineData("abac", false)]
         [InlineData("a", true)]
